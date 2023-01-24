aa7f03d9decef63d707f020c362302faa6f3fd87
Naushad <17naushad2002@gmail.com>
2023-01-24 10:40:37 +0530

    initial push

---
+++ instagram/src/components/DisplayData.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useEffect } from 'react'
+
+function DisplayData() {
+	const [display, setDisplay] = useState();
+	
+
+	useEffect(()=>{
+         fetch("")
+		 .then()
+		 .then()
+		 .catch((err)=>{
+			console.log(err)
+		 })
+	},[])
+
+
+  return (
+	<div>
+	  
+	</div>
+  )
+}
+
+export default DisplayData
---
+++ instagram/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import navbar from "../style/navbar.css"
+
+function Navbar() {
+  return (
+	<div>
+	  <div className='main'>
+         <h1 className='name'>Instagram</h1>
+		 <button className='loginbutton'>Login</button>
+	  </div>
+	</div>
+  )
+}
+
+export default Navbar
